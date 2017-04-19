145bb7f5bd5d0f7be48b01737398452324cc5677
杨柳 <1545766219@qq.com>
2017-04-19 20:13:41 +0800

    添加index.css样式

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div id="root">
+            <h1>hi webpack:</h1>
+            <p>很高兴可以学习到新东西，这是我的第一次尝试哟！</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
